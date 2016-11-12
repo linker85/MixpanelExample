989dc91a93ca956742a91f4e8e549721be4bdaa5
linker85 <linker85@hotmail.com>
2016-11-12 17:18:32 -0500

    Mixpanel example

---
+++ build/generated/source/buildConfig/androidTest/debug/com/mixpanel/example/hello/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mixpanel.example.hello.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mixpanel.example.hello.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
